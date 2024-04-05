5f839feabce1bbace1b0635bc4f6444aff5e68d5
Hijbullah Mahmud <hijbullahmahmud14@gmail.com>
2024-04-06 02:23:14 +0600

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Hijbullah Al Mahmud on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
